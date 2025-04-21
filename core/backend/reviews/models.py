525ca15b611bbdd9dec536d0b37e35825555e9c3
Mujtaba-Syed <mujtaba3198@gmail.com>
2025-04-22 03:22:47 +0500

    add active field and review_images and remove dublicate in .gitignore

--- core/backend/reviews/models.py
+++ core/backend/reviews/models.py
@@ -8,11 +8,13 @@ from django.core.exceptions import ValidationError
 
 class Review(models.Model):
     name = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+    image = models.ImageField(upload_to='review_images/', blank=True, null=True)
     description = models.TextField()
     rating = models.PositiveSmallIntegerField() 
+    is_active = models.BooleanField(default=True) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
     def clean(self):
         if self.rating < 1 or self.rating > 5:
